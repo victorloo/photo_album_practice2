81079990a51017786ed2c2cd14df2bb814de561b
ila9970 <ila9970@gmail.com>
2018-01-28 22:54:07 +0800

    complete create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,22 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+
+    redirect_to photos_url
+  end
+ 
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :date, :description, :file_location)
+  end
+
 end
